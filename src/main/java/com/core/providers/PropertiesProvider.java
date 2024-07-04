8dcee1347d1c449cfda4cf20ebef76ce4f5fffeb
AnnaLevitskii <99040149+AnnaLevitskii@users.noreply.github.com>
2024-07-04 15:49:25 +0300

    refactoring

--- src/main/java/com/core/providers/PropertiesProvider.java
+++ src/main/java/com/core/providers/PropertiesProvider.java
@@ -14,29 +14,25 @@ public class PropertiesProvider implements Path {
      */
     public static String getProperty(String key){
         Properties properties = new Properties();
-        try {
-            FileInputStream fileInputStream = new FileInputStream("src/main/java/com/core/utils/configuration.properties");
+        try (FileInputStream fileInputStream = new FileInputStream("src/main/java/com/core/utils/configuration.properties")) {
             properties.load(fileInputStream);
             return properties.getProperty(key);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
-
         }
     }
     public static void setProperty(String key, String value){
         Properties properties = new Properties();
-        try {
-            FileInputStream fileInputStream = new FileInputStream("src/main/java/com/core/utils/configuration.properties");
+        try (FileInputStream fileInputStream = new FileInputStream("src/main/java/com/core/utils/configuration.properties");
+             FileOutputStream fileOutputStream = new FileOutputStream("src/main/java/com/core/utils/configuration.properties")) {
             properties.load(fileInputStream);
             properties.setProperty(key, value);
-            FileOutputStream fileOutputStream = new FileOutputStream("src/main/java/com/core/utils/configuration.properties");
             properties.store(fileOutputStream, null);
-            fileOutputStream.close();
+
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
-
         }
     }
 }
