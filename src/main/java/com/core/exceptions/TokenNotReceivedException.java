890554ba0cc4a72348c921db1abb2631786ff33a
AnnaLevitskii <AnnaLevitskii@users.noreply.github.com>
2024-09-05 15:24:52 +0000

    deploy: 0d0726593f9aedba029494b94853e4a52a7ae298

--- src/main/java/com/core/exceptions/TokenNotReceivedException.java
+++
@@ -1,8 +0,0 @@
-package com.core.exceptions;
-
-public class TokenNotReceivedException extends Exception{
-    public TokenNotReceivedException(){
-        super("Token not received \n" +
-                "The API is probably dead \n" );
-    }
-}
--- src/main/java/com/core/utils/Path.java
+++
@@ -1,7 +0,0 @@
-package com.core.utils;
-
-public interface Path {
-    String SERIALIZE_PATH = "src/main/resources/serialize/";
-    String PROPERTIES_PATH = "src/main/resources/properties.properties";
-    String XML_PROPERTIES_PATH = "src/main/resources/xmlproperties/";
-}
